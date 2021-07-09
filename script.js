091ef29c49dcb30194cab0b6dc1ff82c27865631
KikoPT1234 <39025735+KikoPT1234@users.noreply.github.com>
2021-07-09 17:27:13 +0200

    landing page done

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.body.style.overflowY = "hidden";
 let i = 0;
 
 const interval = setInterval(() => {
+    window.scrollTo(0, 0);
     i++;
     titleDiv.style.backgroundPositionY = `${i * 4.16667}%`;
     if (i >= 24) clearInterval(interval);
@@ -22,23 +23,28 @@ setTimeout(() => {
     setTimeout(() => {
         document.body.style.overflowY = "auto";
         overlay.style.display = "none";
+        title.style.zIndex = "30";
+        titleDiv.style.zIndex = "30";
+        3;
     }, 400);
 }, 2500);
 
 const banner = document.querySelector(".home-banner");
 
-const img = document.querySelector("img");
-const textContainer = document.querySelector(
-    ".home-banner .banner-text-wrapper .container"
-);
+const img = document.querySelector(".home-banner img");
+const textContainer = document.querySelector(".title div");
 
 document.addEventListener("scroll", (event) => {
     if (window.scrollY >= 40) header.classList.add("scrolled");
     else header.classList.remove("scrolled");
 
     img.style.top = window.scrollY / 1.3 + "px";
-    // textContainer.style.top = window.scrollY / 1.9 + "px";
-    banner.style.filter = `opacity(${
-        1 - window.scrollY / window.innerHeight
-    }) blur(${(window.scrollY / window.innerHeight) * 3}px)`;
+    textContainer.style.top = window.scrollY / 1.9 + "px";
+
+    const filter = `opacity(${1 - window.scrollY / window.innerHeight}) blur(${
+        (window.scrollY / window.innerHeight) * 3
+    }px)`;
+
+    banner.style.filter = filter;
+    textContainer.style.filter = filter;
 });
